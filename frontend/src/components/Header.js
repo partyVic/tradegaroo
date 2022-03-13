fe4411d697be5761da41e8172367931ae19b6ca3
partyVic <vicwang128@gmail.com>
2022-03-13 20:53:01 +1100

    Add react-router-dom V6 & NOT using react-router-bootstrap, use as attribute instead

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Navbar, Container, Nav } from 'react-bootstrap'
 
 const Header = () => {
@@ -6,12 +7,19 @@ const Header = () => {
         <header>
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">Meegle</Navbar.Brand>
+
+                {
+                /*  use the "as" attribute to let Link instead of the original href attribute in Bootstrap components
+                    this will prevent page refresh
+                    and will NOT change the default Bootstrap styles
+                    if use <Link> to wrap Bootstrap components, it may change the default styles
+                */}
+                    <Navbar.Brand as={Link} to="/">Meegle</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-                            <Nav.Link href="/login"><i className='fas fa-user'></i> Sign in</Nav.Link>
+                            <Nav.Link as={Link} to="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+                            <Nav.Link as={Link} to="/login"><i className='fas fa-user'></i> Sign in</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
