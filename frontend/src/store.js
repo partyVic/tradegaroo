33a473cc8021aae01cf6358dba5ec955ef09614b
partyVic <vicwang128@gmail.com>
2022-05-03 18:27:19 +1000

    Create product list reducer & action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,13 @@
 //createStore is deprecated, use configureStore instead
 import { configureStore } from '@reduxjs/toolkit'
+import { productListReducer } from './reducers/productReducers'
 
 const initialState = {}
 
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        productList: productListReducer
+    },
     initialState,
     middleware: [],
     devTools: process.env.NODE_ENV !== 'production', //only show devTools when in production
