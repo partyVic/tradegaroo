588d38a720dd48e1d2710b35437a3da08fe25195
partyVic <vicwang128@gmail.com>
2022-05-06 14:23:56 +1000

    Create qty select option with Array.keys().map & Add to cart button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   return (
@@ -13,6 +14,10 @@ function App() {
         <Container>
           <Routes>
             <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path='/cart'>
+              <Route path=':id' element={<CartScreen />} />
+              <Route path='' element={<CartScreen />} />
+            </Route>
             <Route path="/" element={<HomeScreen />} />
           </Routes>
         </Container>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link, useParams } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Link, useParams, useNavigate } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
@@ -9,6 +9,9 @@ import { listProductDetails } from '../actions/productActions'
 
 const ProductScreen = () => {
     const params = useParams()
+    const navigate = useNavigate()
+
+    const [qty, setQty] = useState(1)
 
     const dispatch = useDispatch()
     const productDetails = useSelector(state => state.productDetails)
@@ -17,6 +20,10 @@ const ProductScreen = () => {
     useEffect(() => {
         dispatch(listProductDetails(params.id))
     }, [dispatch])
+
+    const addToCartHandler = () => {
+        navigate(`/cart/${params.id}?qty=${qty}`)
+    }
 
     return (
         <>
@@ -71,10 +78,33 @@ const ProductScreen = () => {
                                             </Col>
                                         </Row>
                                     </ListGroup.Item>
+
+                                    {product.countInStock > 0 && (
+                                        <ListGroup.Item>
+                                            <Row>
+                                                <Col>Qty</Col>
+                                                <Col>
+                                                    <Form.Control
+                                                        className='form-select'
+                                                        style={{ padding: "0.75rem 0.2rem" }}
+                                                        as='select'
+                                                        value={qty}
+                                                        onChange={e => setQty(e.target.value)}
+                                                    >
+                                                        {[...Array(product.countInStock).keys()].map(x => (
+                                                            <option key={x + 1} value={x + 1}>{x + 1}</option>
+                                                        ))}
+                                                    </Form.Control>
+                                                </Col>
+                                            </Row>
+                                        </ListGroup.Item>
+                                    )}
+
                                     <ListGroup.Item>
                                         <Button
                                             className='w-100'
                                             disabled={product.countInStock === 0}
+                                            onClick={addToCartHandler}
                                         >
                                             Add To Cart
                                         </Button>
