4c13a7d9ff411daef5b49a24035650119fc3410d
partyVic <vicwang128@gmail.com>
2022-05-12 18:38:05 +1000

    Clean up by using controllers for productRoutes

---
+++ backend/controllers/productController.js
@@ -0,0 +1,33 @@
+// handle async error 
+// Simple middleware for handling exceptions inside of async express routes and passing them to your express error handlers
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+})
+
+
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
+const getProductById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        res.json(product)
+    } else {
+        // res.status(404).json({ message: 'Product not found' }) // use this before we create custom error handler
+
+        // use this after we created our custom error handlers
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
+export { getProducts, getProductById }
