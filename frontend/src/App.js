f68873ef6c8a87947888d0a760c0e19d59f2e167
partyVic <vicwang128@gmail.com>
2022-05-15 22:51:13 +1000

    Create user login screen & make it functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <main className='py-3'>
         <Container>
           <Routes>
+            <Route path="/login" element={<LoginScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path='/cart'>
               <Route path=':id' element={<CartScreen />} />
